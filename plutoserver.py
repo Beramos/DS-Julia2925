cc43233e21d1a7439be3ab541368e75d7f19e3cd
BramDeJaegher <bram.de.jaegher@gmail.com>
2020-11-27 13:30:13 +0100

    Update plutoserver.py

--- plutoserver.py
+++ plutoserver.py
@@ -1,6 +1,6 @@
 def setup_plutoserver():
   return {
-    "command": ["julia", "--optimize=0", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
+    "command": ["julia", "--optimize=0", "-e","import Pkg; Pkg.add(\"https://github.com/Beramos/Pluto.jl#DS-Julia-2925\")" ,"import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
     "timeout": 60,
     "launcher_entry": {
         "title": "Pluto.jl",
