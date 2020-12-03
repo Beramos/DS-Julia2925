ed0ffaf6d2f691ff95ac791330f70fcad9135f89
beramos <bram.de.jaegher@gmail.com>
2020-12-03 19:46:03 +0100

    adapt tutorial

--- notebooks/templates/template.jl
+++ notebooks/templates/template.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.4
+# v0.12.12
 
 using Markdown
 using InteractiveUtils
@@ -28,11 +28,102 @@ md"""
 First of all, **_welcome to the course!_**
 """
 
-# ╔═╡ 094e39c8-e6ce-11ea-131b-07c4a1199edf
-
-
 # ╔═╡ 31a8fbf8-e6ce-11ea-2c66-4b4d02b41995
 
+
+# ╔═╡ 79728490-3582-11eb-1c73-eb6c3d13fd7c
+md"### Types of admonition"
+
+# ╔═╡ 6c54a3c6-358a-11eb-3529-05cec9ffa342
+begin
+	ingredients("../../src/DS-Julia2925.jl")
+	
+end
+
+# ╔═╡ 78c3aede-3582-11eb-1de3-27b420b17f46
+hint(text) = Markdown.MD(Markdown.Admonition("hint", "Hint", [text])); 
+
+# ╔═╡ d84b371e-3582-11eb-3176-7faf85f87aea
+hint(md"This is a hint admonition")
+
+# ╔═╡ 44664a2e-3583-11eb-0f41-970201b67453
+correct(text=md"Well done!") = 
+	Markdown.MD(Markdown.Admonition("correct", "Correct", [text]));
+
+# ╔═╡ 80fc23f0-3583-11eb-3dae-a5263b2005f7
+correct()
+
+# ╔═╡ 0f7275d0-3584-11eb-26c0-3f8dceb6c21a
+incorrect(text=md"Keep working on it !") = Markdown.MD(Markdown.Admonition("warning", "Incorrect", [text]));
+
+# ╔═╡ 65a85438-3584-11eb-296b-e3de9deb42d3
+incorrect()
+
+# ╔═╡ 72dd1b16-3584-11eb-02bc-21c80993aca7
+incorrect(md"did not return a number. Did you forget to write `return`?")
+
+# ╔═╡ bf879432-3584-11eb-3a03-e1c39f717173
+fyi(text) = Markdown.MD(
+	Markdown.Admonition("info",
+		"Additional info",
+		[html"""
+			<style> pluto-output div.admonition.info .admonition-title {
+						background: rgb(161, 161, 161);
+					} 
+
+					pluto-output div.admonition.info {
+						background: rgba(161, 161, 161, 0.2);
+						border: 5px solid rgb(161, 161, 161);
+					}
+			</style>
+		""",
+			text
+		]
+	)
+)
+
+# ╔═╡ 2c5bd532-3585-11eb-0e9a-2bf939c940c3
+fyi(md"""
+	Blue is a color 
+	<div> </div>
+	
+	""")
+
+# ╔═╡ 571c8d48-3585-11eb-10da-d732c9561878
+fyi(md"These function can be loaded by using `using ....`")
+
+# ╔═╡ cd897e6e-3587-11eb-19d0-3fff277a2d79
+wtf(text) = Markdown.MD(
+	Markdown.Admonition("wtf",
+		"Self destruct warning",
+		[html"""
+			<style> pluto-output div.admonition.wtf .admonition-title {
+						background: rgb(226, 157, 148);
+						animation:blinkingBox 1.5s infinite;
+					} 
+
+					pluto-output div.admonition.wtf {
+						background: rgba(226, 157, 148, 0.2);
+						border: 5px solid rgb(226, 157, 148);
+						animation:blinkingBox 1.5s infinite;
+					}
+
+			@keyframes blinkingBox{
+				0%{     visibility: hidden;    }
+				30%{    visibility: hidden; }
+				31%{    visibility: visible; }
+				99%{    visibility: visible;  }
+				100%{   visibility: hidden;     }
+			}
+			</style>
+		""",
+			text
+		]
+	)
+)
+
+# ╔═╡ e9e4ad0e-3587-11eb-1d86-b9054c8f634d
+wtf(md"I could not resist, sorry!")
 
 # ╔═╡ 56866718-e6ce-11ea-0804-d108af4e5653
 md"### Open Question
@@ -59,11 +150,14 @@ Write a function newton_sqrt(x) which implements the above algorithm."
 
 # ╔═╡ 4896bf0c-e754-11ea-19dc-1380bb356ab6
 function newton_sqrt(x, error_margin=0.01, a=x / 2) # a=x/2 is the default value of `a`
-	return missing # this is wrong, write your code here!
+	return sqrt(x) # this is wrong, write your code here!
 end
 
 # ╔═╡ 7a01a508-e78a-11ea-11da-999d38785348
 newton_sqrt(2)
+
+# ╔═╡ 713b0250-3582-11eb-21c0-cdc001fc35ee
+
 
 # ╔═╡ 682db9f8-e7b1-11ea-3949-6b683ca8b47b
 let
@@ -104,14 +198,28 @@ md"""
 # ╟─cdff6730-e785-11ea-2546-4969521b33a7
 # ╠═7308bc54-e6cd-11ea-0eab-83f7535edf25
 # ╠═a2181260-e6cd-11ea-2a69-8d9d31d1ef0e
-# ╟─094e39c8-e6ce-11ea-131b-07c4a1199edf
 # ╟─31a8fbf8-e6ce-11ea-2c66-4b4d02b41995
+# ╠═79728490-3582-11eb-1c73-eb6c3d13fd7c
+# ╠═6c54a3c6-358a-11eb-3529-05cec9ffa342
+# ╠═78c3aede-3582-11eb-1de3-27b420b17f46
+# ╠═d84b371e-3582-11eb-3176-7faf85f87aea
+# ╠═44664a2e-3583-11eb-0f41-970201b67453
+# ╠═80fc23f0-3583-11eb-3dae-a5263b2005f7
+# ╠═0f7275d0-3584-11eb-26c0-3f8dceb6c21a
+# ╠═65a85438-3584-11eb-296b-e3de9deb42d3
+# ╠═72dd1b16-3584-11eb-02bc-21c80993aca7
+# ╠═bf879432-3584-11eb-3a03-e1c39f717173
+# ╠═2c5bd532-3585-11eb-0e9a-2bf939c940c3
+# ╠═571c8d48-3585-11eb-10da-d732c9561878
+# ╠═cd897e6e-3587-11eb-19d0-3fff277a2d79
+# ╠═e9e4ad0e-3587-11eb-1d86-b9054c8f634d
 # ╟─56866718-e6ce-11ea-0804-d108af4e5653
 # ╠═bccf0e88-e754-11ea-3ab8-0170c2d44628
 # ╟─e7abd366-e7a6-11ea-30d7-1b6194614d0a
 # ╟─d62f223c-e754-11ea-2470-e72a605a9d7e
 # ╠═4896bf0c-e754-11ea-19dc-1380bb356ab6
 # ╠═7a01a508-e78a-11ea-11da-999d38785348
+# ╠═713b0250-3582-11eb-21c0-cdc001fc35ee
 # ╠═682db9f8-e7b1-11ea-3949-6b683ca8b47b
 # ╠═088cc652-e7a8-11ea-0ca7-f744f6f3afdd
 # ╟─c18dce7a-e7a7-11ea-0a1a-f944d46754e5
