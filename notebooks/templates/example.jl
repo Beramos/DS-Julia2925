15de8b03ae245a7e63bab8a94e8aea588b6051d6
beramos <bram.de.jaegher@gmail.com>
2020-12-27 16:42:59 +0100

    updating example :pencil2:

--- notebooks/templates/example.jl
+++ notebooks/templates/example.jl
@@ -25,20 +25,8 @@ tracker = ProgressTracker(student.name, student.email);
 # ╔═╡ 4707f4ae-3fdc-11eb-3ebd-2b483041d459
 md"-----"
 
-# ╔═╡ 24ca0562-4851-11eb-20fd-bd04c7b49b98
-
-
-# ╔═╡ 2f51df02-3fda-11eb-0f9c-9b5bea842137
-md"## 1. Example simple exercise"
-
-# ╔═╡ 854891dc-2dab-11eb-2b4b-e129081aacca
-md"""Complete the function `myclamp(x)` that clamps a number `x` between 0 and 1.
-
-Open assignments always return `missing`.
-"""
-
 # ╔═╡ e27e6aa0-2dab-11eb-3ccc-43c68f37114b
-myclamp(x) = max(0, min(x,1))
+myclamp(x) = max.(0, min.(x,1))
 
 # ╔═╡ 9bbee332-4170-11eb-05a6-4998f14d307e
 begin
@@ -58,10 +46,18 @@ begin
 		validators= @safe[myclamp([2.0, 0.3])==[1.0, 0.3]]
 	)
 	
+	q₃ = QuestionOptional{Hard}(;
+		description=md"""
+		This is an almost impossible question
+		""",
+		validators= @safe[false]
+	)
+	
 
    qb = QuestionBlock(;
 	title=md"### Question 1.0: What a crazy exercise",
-	questions = [q₁, q₂],
+	description=md" Some additional general kind off description and all.",
+	questions = [q₁, q₂, q₃],
 	hints=[	hint(md"Have you tried this?"),
 			hint(md"Have you tried switching it on and off again?")]
 	);
@@ -73,7 +69,7 @@ end
 myclamp(1.1)
 
 # ╔═╡ e05ee714-3fde-11eb-0209-e30f44655997
-md"The answer checking happens in the second argument of `check_answer()` and by passing the tracker-instance in the first argument the state of the question (correct/incorrect) is tracked"
+md"The automatic grading happens in the second argument of `validate()` and by passing the tracker-instance in the first argument the state of the question (correct/incorrect) is tracked"
 
 # ╔═╡ 221b27f8-3fdf-11eb-06c1-f97060a68bca
 md"**Hints:**"
@@ -87,6 +83,9 @@ md"**FYI's:**"
 # ╔═╡ 3cd3e814-3fdf-11eb-1918-75993ba2e121
 fyi(md"Did you know that scientists recently discovered pink UV-fluorescent squirrels?")
 
+# ╔═╡ 1befa358-485a-11eb-1195-f3f53e48020b
+bomb()
+
 # ╔═╡ Cell order:
 # ╟─d00b3712-3fd6-11eb-354e-3182c3cb8eb1
 # ╠═cfea9340-3fd6-11eb-26cc-57e76c7353cd
@@ -96,13 +95,11 @@ fyi(md"Did you know that scientists recently discovered pink UV-fluorescent squi
 # ╠═1a6a2844-3fdd-11eb-2931-dd7fca88e484
 # ╠═4707f4ae-3fdc-11eb-3ebd-2b483041d459
 # ╠═9bbee332-4170-11eb-05a6-4998f14d307e
-# ╟─24ca0562-4851-11eb-20fd-bd04c7b49b98
-# ╟─2f51df02-3fda-11eb-0f9c-9b5bea842137
-# ╠═854891dc-2dab-11eb-2b4b-e129081aacca
 # ╠═e27e6aa0-2dab-11eb-3ccc-43c68f37114b
 # ╠═87e6c2a8-2dac-11eb-33d3-77a35fc13d71
-# ╟─e05ee714-3fde-11eb-0209-e30f44655997
+# ╠═e05ee714-3fde-11eb-0209-e30f44655997
 # ╟─221b27f8-3fdf-11eb-06c1-f97060a68bca
 # ╠═542d5fa6-2da9-11eb-1037-3b35a5b22bd5
 # ╟─2e7109a0-3fdf-11eb-28af-0d99a01c5066
 # ╠═3cd3e814-3fdf-11eb-1918-75993ba2e121
+# ╠═1befa358-485a-11eb-1195-f3f53e48020b
