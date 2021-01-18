2a983bbc1a5009137afaaabe08cc70131fdbf6ec
beramos <bram.de.jaegher@gmail.com>
2021-01-19 00:15:08 +0100

    day1 add remaining solutions

--- src/solutions.jl
+++ src/solutions.jl
@@ -19,3 +19,45 @@ end
 ### Stirling
 stirling(n) = sqrt(2*π*n)*(n/exp(1))^n
 
+### Time is relative
+function since_epoch(t)
+  now = t
+  seconds = now % 60 # 60 seconds in a minute
+  now -= seconds
+  # to minutes
+  now = div(now, 60) 
+  minutes = now % 60 # 60 minutes in an hour
+  now -= minutes
+  # to hours
+  now = div(now, 60)
+  hours = now % 24 # 24 hours in a day
+  now -= hours
+  # to days
+  now = div(now, 24)
+  days = now % 365 # ± 365 days in a year
+  return days, hours, minutes, seconds
+end
+
+### Fermat
+function checkfermat(a::Int, b::Int, c::Int, n::Int)
+  if a^n + b^n == c^n
+      println("Holy smokes, Fermat was wrong!")
+  else
+      println("No, that doesn’t work.")
+  end
+end
+
+
+### Justify
+rightjustify(s) = println(" "^(70-length(s)) * s)
+
+### Grid print
+function printgrid()
+  dashed = " -"^4*" "
+  plusdash = "+"*dashed*"+"*dashed*"+"
+  verticals = ("|"*" "^9)^2*"|"
+  s = (plusdash * "\n" * (verticals * "\n")^4)^2*plusdash * "\n"
+  println(s)
+end
+
+# WIP bigprint
