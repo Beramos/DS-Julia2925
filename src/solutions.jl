1e5bb263bf7363b370ced07b09a8be3d021c8c79
beramos <bram.de.jaegher@gmail.com>
2021-01-18 23:08:32 +0100

    init solutions module :boom:

---
+++ src/solutions.jl
@@ -0,0 +1,13 @@
+#=
+Created on Monday 18th of January 2021
+Last update: -
+
+@author: Bram De Jaegher
+bram.de.jaegher@gmail.com
+=#
+
+# --- Solutions --- #
+module Solutions
+  include("../solutions/day1.jl")
+  include("../solutions/day2.jl")
+end
