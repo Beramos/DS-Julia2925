6a6fdf2ff8c4b846ec3d325b334ad918bd54c7ff
beramos <bram.de.jaegher@gmail.com>
2021-01-18 23:43:49 +0100

    day 1: add solution of clip function

--- src/solutions.jl
+++ src/solutions.jl
@@ -5,3 +5,12 @@ Last update: -
 @author: Bram De Jaegher
 bram.de.jaegher@gmail.com
 =#
+
+## Day 1
+### Clipping
+
+function clip(x)
+  if x ≤ 0 && return 0  
+  elseif x ≥ 1 && return 1  
+  return x
+end
