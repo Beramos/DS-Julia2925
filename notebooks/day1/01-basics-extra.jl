6e613b40c9dfdab4b1c9b4d49767df408db944ad
beramos <bram.de.jaegher@gmail.com>
2021-02-01 14:26:42 +0100

    type set, proofread, collapsed all the stuff

--- notebooks/day1/01-basics-extra.jl
+++ notebooks/day1/01-basics-extra.jl
@@ -7,7 +7,7 @@ using InteractiveUtils
 # ╔═╡ 24b76a7c-63dd-11eb-1b78-d5a20557e5cd
 # edit the code below to set your name and UGent username
 
-student = (name = "Jeanette Janssen", email = "Jeanette.Janssen@UGent.be");
+student = (name = "Jim Janssen", email = "Jim.Janssen@UGent.be");
 
 # press the ▶ button in the bottom right of this cell to run your edits
 # or use Shift+Enter
@@ -30,13 +30,13 @@ end
 md"""
 # Additional exercises for basics
 
-First of all, _welcome to these additional exercises, **$(student[:name])**!
+First of all, welcome to these additional exercises, **$(student[:name])**!
+
+This exercise is based on [the advent of code](https://adventofcode.com/), a yearly programming challenge during the advent period.
 """
 
 # ╔═╡ 6c0f2d10-63dd-11eb-2c17-fddf9cd51bfe
 md"""## 1. Trajecting trajectories
-
-Let us start with some backstory for the exercise.
 
 Suppose you find yourself on top of a snowy mountain, sparsely covered with trees. You want to reach the bottom of the mountain using your sled, but the situation you find yourself in is certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
 
@@ -147,12 +147,15 @@ begin
    qb1 = QuestionBlock(;
 	title=md"**Question 1: counting trees**",
 	questions = [q1],
-	hints=[hint(md"The question can be answered without defining your own datatypes and using the all that is available from the `Julia.Base` module"),
+	hints=[hint(md"This question can be answered without defining your own datatypes and using all that is available from the `Julia.Base` module. This can also be completed without knowing a lot about collections. Since this is only explained in the next part of the course. "),
 			hint(md"An alterative solution showcasing the power of Julia will be shown below, it includes some teasers of what is yet to come in this course")
 			],
 	)
 	validate(qb1, tracker)
 end
+
+# ╔═╡ dcac803a-648d-11eb-21f2-d72e56528e92
+
 
 # ╔═╡ fb39928c-63e2-11eb-2b85-15044c04fbe6
 product_all_trees = missing
@@ -182,8 +185,14 @@ What do you get if you multiply together the number of trees encountered on each
 	validate(qb2, tracker)
 end
 
+# ╔═╡ ede6342c-648d-11eb-0712-836637408e53
+
+
 # ╔═╡ b9e97832-63e3-11eb-0e39-b5c9129eabbe
-md"## Convoluted answer to these questions showcasing the 💪️ of Julia"
+md"## Overly engineered solution to these questions showcasing the 💪️ of Julia"
+
+# ╔═╡ 9c015578-648e-11eb-0768-d1e5edb4f65f
+md"This is already some advanced use of Julia, so do not worry if this is not yet fully clear. You will learn about this later in this course."
 
 # ╔═╡ f0f0da0a-63e3-11eb-2065-838c77fc57a6
 begin
@@ -355,36 +364,39 @@ prod([goingdowntheslope(direction..., slope) for direction in slopestyles])
 
 # ╔═╡ Cell order:
 # ╠═24b76a7c-63dd-11eb-1b78-d5a20557e5cd
-# ╠═446c3e4e-63dd-11eb-3300-fb006008ff1f
-# ╠═5368424c-63dd-11eb-3a1b-05a61e266d2b
+# ╟─446c3e4e-63dd-11eb-3300-fb006008ff1f
+# ╟─5368424c-63dd-11eb-3a1b-05a61e266d2b
 # ╟─6c0f2d10-63dd-11eb-2c17-fddf9cd51bfe
 # ╟─e1259736-63df-11eb-0010-3f2fc5719596
 # ╠═36e51b02-63df-11eb-34a5-39e6e0650541
 # ╠═e677ca7e-63df-11eb-186b-8d20826ec916
-# ╠═c34af248-63e3-11eb-2bfd-898ef2cfbea5
-# ╠═424c1470-63e0-11eb-323f-454ac8ad273e
+# ╟─c34af248-63e3-11eb-2bfd-898ef2cfbea5
+# ╟─424c1470-63e0-11eb-323f-454ac8ad273e
 # ╠═4b4bbeb8-63e2-11eb-0ea2-6de494f61dc1
-# ╠═c1206434-63e2-11eb-16c1-d7fafb48bbbe
+# ╟─dcac803a-648d-11eb-21f2-d72e56528e92
+# ╟─c1206434-63e2-11eb-16c1-d7fafb48bbbe
 # ╠═fb39928c-63e2-11eb-2b85-15044c04fbe6
-# ╠═b9e97832-63e3-11eb-0e39-b5c9129eabbe
-# ╠═f0f0da0a-63e3-11eb-2065-838c77fc57a6
-# ╠═ec0aa2c6-63ea-11eb-062a-cdd3e81a743a
+# ╟─ede6342c-648d-11eb-0712-836637408e53
+# ╟─b9e97832-63e3-11eb-0e39-b5c9129eabbe
+# ╟─9c015578-648e-11eb-0768-d1e5edb4f65f
+# ╟─f0f0da0a-63e3-11eb-2065-838c77fc57a6
+# ╟─ec0aa2c6-63ea-11eb-062a-cdd3e81a743a
 # ╠═de9cf7b0-63ea-11eb-12fb-773d79c2463b
-# ╠═f752b934-63ea-11eb-09cc-99172ed4870d
+# ╟─f752b934-63ea-11eb-09cc-99172ed4870d
 # ╠═5c2438ce-63eb-11eb-0892-272fb362a2e8
-# ╠═9cafdef2-63eb-11eb-326a-5d51766d17d5
+# ╟─9cafdef2-63eb-11eb-326a-5d51766d17d5
 # ╠═9371b694-63eb-11eb-30a5-d3542ca8f4e5
 # ╠═18054676-63f0-11eb-1a53-5b65f8477882
-# ╠═73f03a14-63f0-11eb-1a14-17e4e69745af
+# ╟─73f03a14-63f0-11eb-1a14-17e4e69745af
 # ╠═2164576e-63f0-11eb-3e57-4fad123c494b
 # ╠═8eda596a-63f0-11eb-03ba-abd147192428
 # ╠═62dfc3ae-63f0-11eb-350c-b3040f8a6d9b
-# ╠═224c26ce-63ec-11eb-0744-e97f5196972a
+# ╟─224c26ce-63ec-11eb-0744-e97f5196972a
 # ╠═9ec3845e-63ec-11eb-1a81-c141b4ff37d8
 # ╠═b590457a-63ec-11eb-3b3b-5d5b0de1f98c
-# ╠═a0f50fee-63ed-11eb-0208-0b5f7036a7b1
+# ╟─a0f50fee-63ed-11eb-0208-0b5f7036a7b1
 # ╠═ae58f724-63eb-11eb-0541-3584d99d9fc5
-# ╠═44885292-63ee-11eb-048b-2b7481a83ae8
+# ╟─44885292-63ee-11eb-048b-2b7481a83ae8
 # ╠═10e183ca-63ec-11eb-3d4b-57ed0543cda1
 # ╠═dd716abc-63ed-11eb-36e9-fdbd1187146e
 # ╠═e0569568-63ed-11eb-33b9-51a8b179292c
