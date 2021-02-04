3e6d731e69f054f0726773f017efa7aa566521c4
michielstock <michielfmstock@gmail.com>
2021-02-04 13:14:30 +0100

    ✅ test vandermonde

--- test/typesystem.jl
+++ test/typesystem.jl
@@ -10,5 +10,12 @@
         @test Solutions.moneyinknuts(Solutions.WizCur(1, 1, 1)) == 1 + 29 + 17 * 29
         @test Solutions.WizCur(1, 1, 1) + Solutions.WizCur(0, 17, 30) == Solutions.WizCur(2, 2, 2)
     end
-end
 
+    @testset "Vandermonde" begin
+        using LinearAlgebra
+        α = [1, 2, 3, 4]
+        V = Solutions.Vandermonde(α, 4)
+        @test V == [ones(Int, 4) α α.^2 α.^3 ]
+        @test det(V) == det([ones(Int, 4) α α.^2 α.^3 ])
+    end
+end
