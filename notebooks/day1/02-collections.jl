d4b64c83d361b5be68dc4a63f17159fba4a1758d
beramos <bram.de.jaegher@gmail.com>
2025-06-25 21:25:14 +0200

    reduce size x to 3 (closes #180)

--- notebooks/day1/02-collections.jl
+++ notebooks/day1/02-collections.jl
@@ -1,17 +1,19 @@
 ### A Pluto.jl notebook ###
-# v0.19.37
+# v0.20.10
 
 using Markdown
 using InteractiveUtils
 
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
-    quote
+    #! format: off
+    return quote
         local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
+    #! format: on
 end
 
 # ╔═╡ cdff6730-e785-11ea-2546-4969521b33a7
