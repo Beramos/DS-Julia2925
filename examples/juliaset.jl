a36840585c0b1018b980ca3238e48603194d4f1b
michielstock <michielfmstock@gmail.com>
2025-07-04 14:55:10 +0200

    :tada: Julia set

---
+++ examples/juliaset.jl
@@ -0,0 +1,32 @@
+# Julia Set Visualization
+#
+# This script generates and plots a Julia fractal using the escape time algorithm
+# for a given complex constant `c`. It colors points based on how quickly their
+# orbits escape to infinity under iteration of f(z) = z^2 + c.
+#
+# Author: Michiel Stock
+# Date: 2025-07-04
+
+using Plots
+
+R = 50.0  # escape radius
+dx = 1e-4
+dy = dx
+
+c = -0.8 + 0.156im
+f = z -> z^2 + c   # single map
+
+
+function escape_time(z, max_iter=500, R=R)
+    for i in 1:max_iter
+        z = f(z)
+        abs(z) > R && return i
+    end
+    return missing
+end
+
+Z = [x + y*im for x in -2:dx:2, y in -2:dy:2]
+
+T = escape_time.(Z)
+
+heatmap(log.(T), framestyle = :none, color=:Paired_12)
